145984acf718a90e3a0e7ccb3d8245854ee5737c
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-02 13:24:27 +0330

    add snowpack react templete 2

--- src/index.jsx
+++ src/index.jsx
@@ -1,26 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
- // Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
- if (import.meta.hot) {
-    import.meta.hot.accept();
-  }
-
-// import {helloWorld} from './hello-world'
-
-// helloWorld();
-
-// import confetti from 'canvas-confetti';
-// confetti.create(document.getElementById('canvas'), {
-//   resize: true,
-//   useWorker: true,
-//  })({ particleCount: 200, spread: 200 });
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (import.meta.hot) {
+  import.meta.hot.accept();
+}
