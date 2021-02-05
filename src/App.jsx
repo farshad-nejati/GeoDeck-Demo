a4bccd02f9fda7042e4f36618f655dcf7f4d8fba
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-05 19:16:10 +0330

    create breakpoint mixin in less

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import logo from '@view/logo/logo.svg';
-import { DatePicker } from 'antd';
-import { Button } from 'antd';
+import '@less/main.less';
 // import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-
-
-
-import '@less/style.less';
 
 function App() {
   return (
     <div className="App">
-      <DatePicker />
-      <Button type="primary">Button</Button>
     </div>
   );
 }
