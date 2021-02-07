f301c27cbeab46cd749418b1542c4725a3af4ed5
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-07 12:20:52 +0330

    change antd card styles to expected styles

--- src/components/auth-user/login.jsx
+++ src/components/auth-user/login.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Button, Col, Row } from 'antd';
+import { Avatar, Button, Card, Col, Row, Skeleton } from 'antd';
 import { Link } from 'react-router-dom';
 import Title from 'antd/lib/typography/Title';
+import Meta from 'antd/lib/card/Meta';
 
 const Login = (props) => {
   console.log(props);
   return (
-    <div style={{padding: "16px"}}>
+    <div style={{ padding: '16px' }}>
       <p>Login</p>
       {/* <DatePicker /> */}
       <br />
@@ -19,7 +20,7 @@ const Login = (props) => {
         <Title level={5}>h5. Ant Design</Title>
       </div>
 
-      <Row style={{padding:"16px 0"}}>
+      <Row style={{ padding: '16px 0' }}>
         <Col className="gutter-row" span={6}>
           <Button type="primary">primary</Button>
           <Button type="link">link</Button>
@@ -31,6 +32,26 @@ const Login = (props) => {
         <Col className="gutter-row" span={6}></Col>
         <Col className="gutter-row" span={6}></Col>
         <Col className="gutter-row" span={6}></Col>
+      </Row>
+      <Row style={{ padding: '16px 0' }}>
+        <Card
+         bordered={false} 
+          style={{ width: 300, marginTop: 16 }}
+          cover={          
+          <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />
+          }
+
+        >
+          <Skeleton loading={false} avatar active>
+            <Meta
+              avatar={
+                <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+              }
+              title="Card title"
+              description="This is the description"
+            />
+          </Skeleton>
+        </Card>
       </Row>
 
       <Link to="/register">Register</Link>
