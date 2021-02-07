fbc0ca1828129c70ef8aaa8df6a1f0a6c2296bb3
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-07 16:41:47 +0330

    create demo page

--- src/components/auth-user/login.jsx
+++ src/components/auth-user/login.jsx
@@ -1,49 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ModalDemo from '../example/modal-demo';
-import DemoTab from '../example/tabs-demo';
-import CardDemo from '../example/card-demo';
-import ButtonDemo from '../example/button-demo';
-import RouteDemo from '../example/route-demo';
-import TypographyDemo from '../example/typography-demo';
 
 const Login = (props) => {
   console.log(props);
   return (
-    <div
-      style={{
-        gridGap: '5rem',
-        display: 'grid',
-        gridTemplateColumns: '1fr 1fr',
-        alignItems: 'center',
-        justifyItems: 'center',
-        justifyContent: 'center',
-        padding: '32px 8px',
-      }}
-    >
-      <div>
-        <ButtonDemo />
-      </div>
-
-      <div>
-        <RouteDemo />
-      </div>
-
-      <div>
-        <DemoTab />
-      </div>
-
-      <div>
-        <ModalDemo />
-      </div>
-
-      <div>
-        <CardDemo />
-      </div>
-
-      <div>
-        <TypographyDemo />
-      </div>
+     <div>
+       Login page
     </div>
   );
 };
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const Register = React.lazy(() => import('components/auth-user/register'));
 const Main = React.lazy(() => import('components/main-page'));
 const Gallery = React.lazy(() => import('components/gallery-view'));
 const Project = React.lazy(() => import('components/project-page'));
+const Demo = React.lazy(() => import('components/example'));
 
 const routes = [
   { path: '/', exact: true, name:"Login Page", component: Login },
@@ -13,6 +14,7 @@ const routes = [
   { path: '/main/:id', exact: true, name:"Main Page", component: Main },
   { path: '/gallery', exact: true, name:"Gallery Page", component: Gallery },
   { path: '/project', exact: true, name:"Project Page", component: Project },
+  { path: '/demo', exact: true, name:"demo Page", component: Demo },
 ];
 
 export default routes;
