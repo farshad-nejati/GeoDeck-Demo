e7612e24c2bb6c0987634ad0df7fb2ea2a59fd88
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-10 16:02:41 +0330

    create card-gallery classname and fix antd card style

--- src/components/demo-components/card-demo.jsx
+++ src/components/demo-components/card-demo.jsx
@@ -3,26 +3,41 @@ import { Avatar, Button, Card, Col, Row, Skeleton } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 
 const CardDemo = () => (
-  <Card
-    bordered={false}
-    style={{ width: 300, marginTop: 16 }}
-    cover={
-      <img
-        alt="example"
-        src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-      />
-    }
-  >
-    <Skeleton loading={false} avatar active>
-      <Meta
-        avatar={
-          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
-        }
-        title="Card title"
-        description="This is the description"
-      />
-    </Skeleton>
-  </Card>
+  <>
+    <Card
+      bordered={false}
+      style={{ width: 300, marginTop: 16 }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <Skeleton loading={false} avatar active>
+        <Meta
+          avatar={
+            <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          }
+          title="Card title"
+          description="This is the description"
+        />
+      </Skeleton>
+    </Card>
+    <Card
+      className="card__gallery"
+      bordered={false}
+      style={{ width: 300, marginTop: 16 }}
+      cover={
+        <img
+          alt="example"
+          src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+        />
+      }
+    >
+      <Meta title="Card title" description="This is the description" />
+    </Card>
+  </>
 );
 
 export default CardDemo;
