60157667e60b7f91c5b4de976d1568499babdca8
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-14 02:09:13 +0330

    add link to main in project card and fix navigation bug

--- src/components/projeect-components/project-card.jsx
+++ src/components/projeect-components/project-card.jsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import { Avatar, Card, Skeleton } from 'antd';
+import { Link } from 'react-router-dom';
+import { Card } from 'antd';
 import Meta from 'antd/lib/card/Meta';
 import { BASE_URL } from '@app/constant';
 
 const ProjectCard = (props) => {
-  const { title, description, source } = props;
+  const { title, description, source, id } = props;
   return (
     <>
-      <Card
-        className="card__project"
-        bordered={false}
-        //   style={{ width: 300, marginTop: 16 }}
-        cover={<img alt={title} src={`${BASE_URL}/project/${source}`} />}
-      >
-        {/* <Skeleton loading={false} avatar active> */}
-        <Meta title={title} description={description} />
-        {/* </Skeleton> */}
-      </Card>
+      <Link to={`/main/${id}`}>
+        <Card
+          className="card__project"
+          bordered={false}
+          //   style={{ width: 300, marginTop: 16 }}
+          cover={<img alt={title} src={`${BASE_URL}/project/${source}`} />}
+        >
+          {/* <Skeleton loading={false} avatar active> */}
+          <Meta title={title} description={description} />
+          {/* </Skeleton> */}
+        </Card>
+      </Link>
     </>
   );
 };
