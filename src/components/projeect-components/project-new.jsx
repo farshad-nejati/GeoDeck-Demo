7dce11a07108070891103a0e7b8bb4b8523d8f95
farshad_nejati <nejati.farshad93@gmail.com>
2021-02-13 21:10:14 +0330

    create project modal

--- src/components/projeect-components/project-new.jsx
+++ src/components/projeect-components/project-new.jsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import { Typography } from 'antd';
+import React, { useState } from 'react';
+import { Typography, Modal } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
+import ProjectModal from './project-modal';
 
 const { Title } = Typography;
 
 const ProjectNew = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
-    <div className="card__project project__new">
-      <div className="project__new__logo">
-        <PlusOutlined />
+    <>
+      <div
+        className="card__project project__new"
+        onClick={() => setModalVisible(true)}
+      >
+        <div className="project__new__logo">
+          <PlusOutlined />
+        </div>
+        <Title level={5} type="secondary">
+          Add New Project
+        </Title>
       </div>
-      <Title level={5} type="secondary">
-        Add New Project
-      </Title>
-    </div>
+      <Modal
+        title="Add New Project"
+        centered
+        visible={modalVisible}
+        onOk={() => setModalVisible(false)}
+        onCancel={() => setModalVisible(false)}
+      >
+        <p>some contents...</p>
+        <p>some contents...</p>
+        <p>some contents...</p>
+      </Modal>
+    </>
   );
 };
 
